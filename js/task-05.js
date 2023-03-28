00cc9ff6a624342ee5227dc47a1a39b89b2b0db1
Darya <daryakolisnik@gmail.com>
2023-03-28 17:39:23 +0300

    исправила 5е задание

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const input = document.querySelector('#name-input');
 let span = document.querySelector('#name-output');
 
 input.addEventListener('input', () => {
-    if (input.value.trim() !== ' ') {
+    if (input.value.trim()) {
         span.textContent = input.value;
     } else {
         span.textContent = 'Anonymous';
