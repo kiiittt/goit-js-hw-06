07b389da119c804201acdbb5f2fecd9f055fbf83
Darya <daryakolisnik@gmail.com>
2023-03-28 15:10:31 +0300

    .

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const button = document.querySelector('button.change-color');
+const colorSpan = document.querySelector('span.color');
+const body = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+}
+
+button.addEventListener('click', changeColor);
